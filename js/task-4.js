77905cac6bbf113c67e40f73b7cae10ec00f2b29
rojhatbalta <rojhatbalta@gmail.com>
2024-06-30 23:28:35 +0300

    remove break from switch case for return

--- js/task-4.js
+++ js/task-4.js
@@ -2,20 +2,15 @@ function getShippingCost(country) {
   switch (country) {
     case "China":
       return `Shipping to China will cost 100 credits`;
-      break;
     case "Chile":
       return `Shipping to Chile will cost 250 credits`;
-      break;
     case "Australia":
       return `Shipping to Australia will cost 170 credits`;
-      break;
     case "Jamaica":
       return `Shipping to Jamaica will cost 120 credits`;
-      break;
 
     default:
       return `Sorry, there is no delivery to your country`;
-      break;
   }
 }
 
